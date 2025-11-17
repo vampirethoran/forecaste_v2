f730b2b8fdf46d9d8251fdcc65d755f469353f77
bijoy <bijoy.beneathatree@gmail.com>
2025-11-17 12:21:34 +0530

    add prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,57 +6,57 @@ import React, { Suspense } from "react";
 import { Special_Elite } from "next/font/google";
 
 const primaryFont = Special_Elite({
-  weight: "400",
-  subsets: ["latin"],
+    weight: "400",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "forecaste",
-  description: "Your app description",
+    title: "forecaste",
+    description: "Your app description",
 };
 
 export const viewport: Viewport = {
-  width: "device-width",
-  initialScale: 1.0,
+    width: "device-width",
+    initialScale: 1.0,
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className="flex flex-col gap-5 items-center justify-center">
-        <div
-          id="app"
-          className="w-[400px] flex relative justify-center items-start flex-col bg-amber-100 border border-black"
-        >
-          <h1
-            id="title"
-            className={`${primaryFont.className} text-lg text-blue-500 p-5 pb-0 mt-1`}
-          >
-            <strong>forecaste</strong>
-          </h1>
-          <div
-            id="game-container"
-            className={`${primaryFont.className} w-full h-[650px] flex flex-col items-center justify-center`}
-          >
-            <Suspense fallback={null}>{children}</Suspense>
-          </div>
-        </div>
-        <div className="flex gap-2">
-          <BasicButton
-            href="/"
-            id="reset-btn"
-            text="Reset Game"
-            className="hover:bg-red-500"
-          />
-          <Suspense fallback={null}>
-            <AudioManager />
-          </Suspense>
-        </div>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className="flex flex-col gap-5 items-center justify-center">
+                <div
+                    id="app"
+                    className="w-full flex relative justify-center items-start flex-col bg-amber-100 border border-black"
+                >
+                    <h1
+                        id="title"
+                        className={`${primaryFont.className} text-lg text-blue-500 p-5 pb-0 mt-1`}
+                    >
+                        <strong>forecaste</strong>
+                    </h1>
+                    <div
+                        id="game-container"
+                        className={`${primaryFont.className} w-full h-[650px] flex flex-col items-center justify-center`}
+                    >
+                        <Suspense fallback={null}>{children}</Suspense>
+                    </div>
+                </div>
+                <div className="flex gap-2">
+                    <BasicButton
+                        href="/"
+                        id="reset-btn"
+                        text="Reset Game"
+                        className="hover:bg-red-500"
+                    />
+                    <Suspense fallback={null}>
+                        <AudioManager />
+                    </Suspense>
+                </div>
+            </body>
+        </html>
+    );
 }
